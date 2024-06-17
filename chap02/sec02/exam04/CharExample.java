93d8d747e28429514414840c89246897e54a5aa5
jujaemin <147119777+jujaemin@users.noreply.github.com>
2024-06-17 17:20:32 +0900

    Add files via upload

---
+++ chap02/sec02/exam04/CharExample.java
@@ -0,0 +1,17 @@
+package sec02.exam04;
+
+public class CharExample {
+
+	public static void main(String[] args) {
+		
+		char var1 = 'A';
+		char c1 = 65;
+		char c2 = '\u0041';
+		
+		System.out.println("var1:"+var1);
+		System.out.println("c1:"+c1);
+		System.out.println("c2:"+c2);
+
+	}
+
+}
---
+++ chap02/sec02/exam07/FloatDoubleExample.java
@@ -0,0 +1,28 @@
+package sec02.exam07;
+
+public class FloatDoubleExample {
+
+	public static void main(String[] args) {
+		
+		float var1 = 3.14f;
+		double var2 = 3.14;
+		
+		float var3 = 0.1234567890123456789f;
+		double var4 = 0.123456789123456789;
+		
+		System.out.println("var1:"+var1);
+		System.out.println("var2:"+var2);
+		System.out.println("var3:"+var3);
+		System.out.println("var4:"+var4);
+		
+		double var5 = 3e6;
+		float var6 = 3e6f;
+		double var7 = 2e-3;
+		
+		System.out.println("var5:"+var5);
+		System.out.println("var6:"+var6);
+		System.out.println("var7:"+var7);
+
+	}
+
+}
